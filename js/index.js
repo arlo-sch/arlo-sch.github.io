5c03d45a01330f1f87a4c89f41e773a8b68f6e02
Evan Schallerer <eschallerer@q5id.com>
2021-04-04 20:44:50 -0700

    add test data switch

--- js/index.js
+++ js/index.js
@@ -5,9 +5,40 @@ var app = new Vue({
 	},
 	created: function () {
 		let _this = this;
-		$.getJSON(
-			"/js/data.json", 
-			function (json) { _this.json = json; console.log(json); }
-		)
+		var host = window.location.hostname;
+		console.log(host);
+		if (host != "arlo-sch.github.io"){
+			_this.json = {
+				"contactInfo": {
+					"phoneNumber": "(503) 555-5555",
+					"emailAddress": "someone@gmail.com",
+					"humanName": "Someone Somewhere",
+					"addressLine1": "1234 Somewhere St.",
+					"addressLine2": "",
+					"city": "Portland",
+					"state": "Oregon",
+					"zipCode": "97211"    
+				},
+				"petInfo": {
+					"petName": "Arlo",
+					"petWeight": "13 lbs",
+					"petDescription": "Chihuaha/terrier mix, all black coat. Friendly, loves to be warm and snuggle.",
+					"petImages": [
+						"arlo1.jpg",
+						"arlo2.jpg",
+						"arlo3.jpg"
+					],
+					"documentImages": [
+						"rabiesTag.jpg"
+					]
+				}
+			};
+		}
+		else {
+			$.getJSON(
+				"/js/data.json", 
+				function (json) { _this.json = json; }
+			);
+		}
 	}
 })
