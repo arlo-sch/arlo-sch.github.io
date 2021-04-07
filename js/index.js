be2503107e615ebcc8782758f1892d00b3a55e32
Evan Schallerer <eschallerer@q5id.com>
2021-04-06 21:00:22 -0700

    Update styling and data.

--- js/index.js
+++ js/index.js
@@ -6,7 +6,15 @@ var app = new Vue({
 	methods: {
 		decryptWithAES(text, key) {
 			return CryptoJS.AES.decrypt(text, key).toString(CryptoJS.enc.Utf8);
-    }
+    },
+		buildGoogleMapsLink() {
+			let destination = `${this.json.contactInfo.addressLine1}`;
+			if(this.json.contactInfo.addressLine2) 
+			 destination += this.json.contactInfo.addressLine2;
+			destination += `${this.json.contactInfo.city}+${this.json.contactInfo.state}`;
+			destination = encodeURIComponent(destination);
+			return `https://www.google.com/maps/dir/?api=1&destination=${destination}`;
+		}
 	},
 	created: function () {
 		let _this = this;
